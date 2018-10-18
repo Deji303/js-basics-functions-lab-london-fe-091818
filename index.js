fda702304681f308b95b28a4c9ba022003b99b8c
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-18 19:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Code your solution in this file!
-let block = function distanceFromHqInBlocks(street) {
+function distanceFromHqInBlocks(street) {
   if (street < 42) {
     return 42 - street;
   } else {return street - 42 ;}
   
 }
 
-block; 
+
 function distanceFromHqInFeet() {
   return block*264;  
 }
