5d6b9e3b97e3d56cce59fe1a7af56d5e6c675285
Ayodeji Okusanya <ayodeji.okusanya@gmail.com>
2018-10-18 19:57:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+
+
+function distanceFromHqInBlocks(street) {
+  if (street < 42) {
+    return 42 - street;
+  } else {return street - 42 ;}
+  
+}
+
+
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street)*264;  
+}
+
+function distanceTravelledInFeet(street1,street2) {
+  if(street1<street2) {
+    return (street2-street1)*264;
+  } else {return (street1-street2)*264}
+}
+
+function calculatesFarePrice(street1, street2) {
+  if (distanceTravelledInFeet(street1,street2) <= 400) {
+  return 0;
+  }
+  else if (distanceTravelledInFeet(street1,street2) > 400 && distanceTravelledInFeet(street1,street2) <2000) {
+  return ((distanceTravelledInFeet(street1,street2)-400)*2)/100 ;
+}
+else if ( distanceTravelledInFeet(street1,street2) > 2000 && distanceTravelledInFeet(street1,street2) < 2500) {
+  return 25;
+}
+
+else {return "cannot travel that far";}
+}
+
+
